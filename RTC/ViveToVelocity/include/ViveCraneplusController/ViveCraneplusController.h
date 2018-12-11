d45d19a330cd1d185ddcf5b70c79e1f601077aae
syokoyama55 <44572272+Shogo-Yokoyama@users.noreply.github.com>
2018-12-11 20:31:47 +0900

    Add files via upload

---
+++ RTC/ViveToVelocity/include/ViveCraneplusController/ViveCraneplusController.h
@@ -0,0 +1,330 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  ViveCraneplusController.h
+ * @brief The controller of CRANE+ with HTC VIVE
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef VIVECRANEPLUSCONTROLLER_H
+#define VIVECRANEPLUSCONTROLLER_H
+
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "ManipulatorCommonInterface_CommonStub.h"
+#include "ManipulatorCommonInterface_MiddleLevelStub.h"
+#include "VIVEStub.h"
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="port_stub_h">
+using namespace ViveControl;
+// </rtc-template>
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+
+enum{
+	GRIPPER_OPEN = 0,
+	GRIPPER_CLOSE = 1,
+};
+
+typedef struct{
+	double x;
+	double y;
+	double z;
+}Pos;
+
+
+using namespace RTC;
+
+/*!
+ * @class ViveCraneplusController
+ * @brief The controller of CRANE+ with HTC VIVE
+ *
+ */
+class ViveCraneplusController
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  ViveCraneplusController(RTC::Manager* manager);
+
+  /*!
+   * @brief destructor
+   */
+  ~ViveCraneplusController();
+
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
+
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   * formaer rtc_init_entry() 
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
+
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   * formaer rtc_exiting_entry()
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   * former rtc_starting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   * former rtc_stopping_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The activated action (Active state entry action)
+   * former rtc_active_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The deactivated action (Active state exit action)
+   * former rtc_active_exit()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The execution action that is invoked periodically
+   * former rtc_active_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   * former rtc_aborting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The error action in ERROR state
+   * former rtc_error_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The reset action that is invoked resetting
+   * This is same but different the former rtc_init_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
+
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  /*!
+   * 
+   * - Name:  Speed
+   * - DefaultValue: 20
+   */
+  int m_Speed;
+  /*!
+   * 使用するVIVEコントローラの指定．最も早く接続されたVIVEコントロ
+   * ーラを0とする
+   * - Name:  controllerIndex
+   * - DefaultValue: 0
+   */
+  int m_controllerIndex;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  ViveControl::TimedViveControllerSeq m_controller;
+  /*!
+   */
+  InPort<ViveControl::TimedViveControllerSeq> m_controllerIn;
+  
+  // </rtc-template>
+
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  /*!
+   */
+  RTC::CorbaPort m_ManipulatorCommonInterface_CommonPort;
+  /*!
+   */
+  RTC::CorbaPort m_ManipulatorCommonInterface_MiddlePort;
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  /*!
+   */
+  RTC::CorbaConsumer<JARA_ARM::ManipulatorCommonInterface_Common> m_ManipulatorCommonInterface_Common;
+  /*!
+   */
+  RTC::CorbaConsumer<JARA_ARM::ManipulatorCommonInterface_Middle> m_ManipulatorCommonInterface_Middle;
+  
+  // </rtc-template>
+
+ private:
+  // <rtc-template block="private_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
+	 JARA_ARM::CarPosWithElbow pos;
+	 JARA_ARM::RETURN_ID_var m_rid;
+
+	 int getDefaultPosFlag;
+	 Pos defaultVivePos;
+
+	 int bfrGripperFlag;
+
+	 void getTargetPos();
+};
+
+
+extern "C"
+{
+  DLL_EXPORT void ViveCraneplusControllerInit(RTC::Manager* manager);
+};
+
+#endif // VIVECRANEPLUSCONTROLLER_H
---
+++ RTC/ViveToVelocity/src/ViveCraneplusController.cpp
@@ -0,0 +1,282 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  ViveCraneplusController.cpp
+ * @brief The controller of CRANE+ with HTC VIVE
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "ViveCraneplusController.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* vivecranepluscontroller_spec[] =
+  {
+    "implementation_id", "ViveCraneplusController",
+    "type_name",         "ViveCraneplusController",
+    "description",       "The controller of CRANE+ with HTC VIVE",
+    "version",           "1.0.0",
+    "vendor",            "rsdlab",
+    "category",          "The robot controller",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.speed", "20",
+    "conf.default.controllerIndex", "0",
+
+    // Widget
+    "conf.__widget__.speed", "text",
+    "conf.__widget__.controllerIndex", "text",
+    // Constraints
+
+    "conf.__type__.speed", "int",
+    "conf.__type__.controllerIndex", "int",
+
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ViveCraneplusController::ViveCraneplusController(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_controllerIn("Controller", m_controller),
+    m_ManipulatorCommonInterface_CommonPort("ManipulatorCommonInterface_Common"),
+    m_ManipulatorCommonInterface_MiddlePort("ManipulatorCommonInterface_Middle")
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ViveCraneplusController::~ViveCraneplusController()
+{
+}
+
+
+
+RTC::ReturnCode_t ViveCraneplusController::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("Controller", m_controllerIn);
+  
+  // Set OutPort buffer
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  m_ManipulatorCommonInterface_CommonPort.registerConsumer("ManipulatorCommonInterface_Common", "JARA_ARM::ManipulatorCommonInterface_Common", m_ManipulatorCommonInterface_Common);
+  m_ManipulatorCommonInterface_MiddlePort.registerConsumer("ManipulatorCommonInterface_Middle", "JARA_ARM::ManipulatorCommonInterface_Middle", m_ManipulatorCommonInterface_Middle);
+  
+  // Set CORBA Service Ports
+  addPort(m_ManipulatorCommonInterface_CommonPort);
+  addPort(m_ManipulatorCommonInterface_MiddlePort);
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("speed", m_Speed, "20");
+  bindParameter("controllerIndex", m_controllerIndex, "0");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t ViveCraneplusController::onActivated(RTC::UniqueId ec_id)
+{
+	//プロバイダより遅くActivateするため
+	Sleep(1000);
+
+	//関節速度の設定
+	JARA_ARM::ULONG spdRation = m_Speed;
+	m_ManipulatorCommonInterface_Middle->setSpeedJoint(spdRation);
+
+	m_rid = m_ManipulatorCommonInterface_Common->servoON();
+	if (m_rid->id != 0){//Error
+		std::cout << "Servo ON ERROR" << std::endl;
+		std::cout << m_rid->comment << std::endl << std::endl;
+	}
+
+	getDefaultPosFlag = true;
+	bfrGripperFlag = GRIPPER_OPEN;
+  
+	return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ViveCraneplusController::onDeactivated(RTC::UniqueId ec_id)
+{
+	m_rid = m_ManipulatorCommonInterface_Common->servoOFF();
+	if (m_rid->id != 0){//Error
+		std::cout << "Servo OFF ERROR" << std::endl;
+		std::cout << m_rid->comment << std::endl << std::endl;
+	}
+  
+	return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ViveCraneplusController::onExecute(RTC::UniqueId ec_id)
+{
+	if (m_controllerIn.isNew())
+	{
+		m_controllerIn.read();
+
+		if (getDefaultPosFlag)
+		{
+			getDefaultPosFlag = false;
+			defaultVivePos.x = m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.x;
+			defaultVivePos.y = m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.y;
+			defaultVivePos.z = m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.z;
+		}
+
+		//グリッパー閉
+		if (bfrGripperFlag == GRIPPER_OPEN && m_controller.data[m_controllerIndex].trigger == 1.0)
+		{
+			m_rid = m_ManipulatorCommonInterface_Middle->closeGripper();
+			if (m_rid->id != 0){//Error
+				std::cout << "closeGripper ERROR" << std::endl;
+				std::cout << m_rid->comment << std::endl << std::endl;
+			}
+		}
+		//グリッパー開
+		else if (bfrGripperFlag == GRIPPER_CLOSE && m_controller.data[m_controllerIndex].trigger != 1.0)
+		{
+			m_rid = m_ManipulatorCommonInterface_Middle->openGripper();
+			if (m_rid->id != 0){//Error
+				std::cout << "openGripper ERROR" << std::endl;
+				std::cout << m_rid->comment << std::endl << std::endl;
+			}
+		}
+		//アーム操作
+		else
+		{
+			getTargetPos();
+		}
+	}
+
+	bfrGripperFlag = (m_controller.data[m_controllerIndex].trigger == 1.0) ? GRIPPER_CLOSE : GRIPPER_OPEN;
+
+  
+	return RTC::RTC_OK;
+}
+
+void ViveCraneplusController::getTargetPos()
+{
+	//姿勢は自動で計算されるため初期化のみ
+	//1列目
+	pos.carPos[0][0] = cos(0);
+	pos.carPos[1][0] = sin(0);
+	pos.carPos[2][0] = 0.0;
+
+	//2列目
+	pos.carPos[0][1] = -sin(0);
+	pos.carPos[1][1] = cos(0);
+	pos.carPos[2][1] = 0.0;
+
+	//3列目
+	pos.carPos[0][2] = 0.0;
+	pos.carPos[1][2] = 0.0;
+	pos.carPos[2][2] = 1.0;
+
+	//4列目
+	// 座標系 CRANE+ : HTC VIVE = X : -Z / Y : -X / Z :  Y
+	pos.carPos[0][3] =
+		250 - (m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.z - defaultVivePos.z) * 1000; //[mm]
+	pos.carPos[1][3] =
+		250 - (m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.x - defaultVivePos.x) * 1000;
+	pos.carPos[2][3] =
+		250 + (m_controller.data[m_controllerIndex].controllerPoseVel.pose.position.y - defaultVivePos.y) * 1000;
+
+	m_rid = m_ManipulatorCommonInterface_Middle->movePTPCartesianAbs(pos);
+	if (m_rid->id != 0){//Error
+		std::cout << "movePTPCartesianAbs ERROR" << std::endl;
+		std::cout << m_rid->comment << std::endl << std::endl;
+	}
+}
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ViveCraneplusController::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void ViveCraneplusControllerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(vivecranepluscontroller_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ViveCraneplusController>,
+                             RTC::Delete<ViveCraneplusController>);
+  }
+  
+};
+
+
